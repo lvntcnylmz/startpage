8496d1e682a82c37a2197f786312b63661dd1a66
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2022-12-01 14:45:38 +0100

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    colors: {
+      'purple': '#120024',
+      'light-purple': '#250c3e',
+      'orange': '#ff6000',
+      'green': '#00a878',
+      'gray': '#c4c0c0',
+      'dark-gray': '#7b6d84'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
