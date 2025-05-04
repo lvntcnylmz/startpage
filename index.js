bcf4f5e01fafba6cf47b52d4b3ae4dd738164080
Levent C. Yilmaz <leventcanyilmaz@proton.me>
2025-05-04 22:47:16 +0300

    refactor: refactor codes

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 document.onreadystatechange = updateClock();
 
 function updateClock() {
-    const date = new Date()
-    document.getElementById("current-time-text").innerText =
-        date.toLocaleTimeString([], { hour12: false }) + " - " + date.toDateString()
-    const hour = date.getHours()
+  const date = new Date();
+  document.getElementById("current-time-text").innerText =
+    date.toLocaleTimeString([], { hour12: false }) +
+    " - " +
+    date.toDateString();
+  const hour = date.getHours();
 }
 
-setInterval(updateClock, 1000)
+setInterval(updateClock, 1000);
 
 function doSearch() {
-    const query = document.getElementById('search-input').value
-    window.location.href = `https://www.duckduckgo.com/?q=${query}`
+  const query = document.getElementById("search-input").value;
+  window.location.href = `https://www.duckduckgo.com/?q=${query}`;
 }
 
-const searchInput = document.getElementById("search-input")
+const searchInput = document.getElementById("search-input");
 searchInput.addEventListener("keypress", function onEvent(event) {
-    if (event.key === "Enter") {
-        document.getElementById("search-button").click();
-    }
-})
+  if (event.key === "Enter") {
+    document.getElementById("search-button").click();
+  }
+});
 
 function getWeatherData(city) {
-    fetch("https://wttr.in/" + city + "?format=%C+%c+%t+%w+%m")
-        .then(response => response.text())
-        .then(data => document.getElementById("weatherData").innerHTML = data)
+  fetch("https://wttr.in/" + city + "?format=%C+%c+%t")
+    .then((response) => response.text())
+    .then((data) => (document.getElementById("weatherData").innerHTML = data));
 }
